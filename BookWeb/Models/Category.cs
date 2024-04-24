19773348de9a2256924f6c2f3e417497d275bed3
AmanGit06 <aman.dhayani1@gmail.com>
2024-04-24 23:02:52 +0530

    Creating Connection String and database and Migration Process

--- BookWeb/Models/Category.cs
+++ BookWeb/Models/Category.cs
@@ -1,6 +1,18 @@
-﻿namespace BookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWeb.Models
 {
     public class Category
     {
+        [Key]
+        public string CatId { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string CatName { get; set; }
+
+        public string DispalyOrder { get; set; }
     }
 }
+
+
