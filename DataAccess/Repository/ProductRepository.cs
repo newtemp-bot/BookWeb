2c19d74c9a06c62487dbc6615ea7ca5170572f35
@AmanGit <aman.dhayani1@gmail.com>
2024-05-26 13:29:21 +0530

    Product Crud Operation

---
+++ DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookWeb.DataAccess.Data;
+using BookWeb.DataAccess.Repository.IRepository;
+using BookWeb.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWeb.DataAccess.Repository
+{
+    public class ProductRepository : Repository <Product>,IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
